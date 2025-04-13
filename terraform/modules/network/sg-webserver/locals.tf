4650be6dd8abe403efce96bf70beb89c13cacc40
Harsh Mishra <hmisraji07@gmail.com>
2025-04-13 13:08:53 +0530

    Add Security Group For Web Servers with HTTPS Enabled
    Signed-off-by: Harsh Mishra <hmisraji07@gmail.com>

---
+++ terraform/modules/network/sg-webserver/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  name = "sg-with-https-${var.vpc_name}"
+  ingress_cidr_blocks = var.ingress_cidr_blocks
+  ingress_rules = concat(["ssh-tcp", "https-443-tcp"], var.enable_http_80 ? ["http-80-tcp"]: [])
+  egress_rules  = ["all-all"]
+  description   = "Security group with HTTPS traffic enabled from public"
+  default_tags = {
+    name        = local.name
+  }
+  ingress_with_cidr_blocks = var.ingress_with_cidr_blocks
+  tags = var.tags != null? merge(var.tags, local.default_tags) : local.default_tags
+}
---
+++ terraform/modules/network/sg-webserver/variables.tf
@@ -0,0 +1,39 @@
+variable "region" {
+  description = "AWS region to deploy our resources"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_id" {
+  description = "vpc id for the sg"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "name of the vpc"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to be applied"
+  type = map(any)
+  default = {}
+}
+
+variable "ingress_cidr_blocks" {
+  description = "List of IPv4 CIDR ranges to use on all ingress rules"
+  type = list(string)
+  default = [ "0.0.0.0/0" ]
+}
+
+variable "ingress_with_cidr_blocks" {
+  description = "List of computed ingress rules to create where 'cidr_blocks' is used"
+  type = list(map(string))
+  default = []
+}
+
+variable "enable_http_80" {
+  description = "Enable Insecure HTTP Traffic on Port 80"
+  type = bool
+  default = true
+}
